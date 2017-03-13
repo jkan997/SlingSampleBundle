0c10dfd127ad1c078af8ffb0ec9fa289d86f6ee1
jakaniew <jakaniew@192.168.213.1>
2017-03-13 13:15:05 +0100

    Book Models update

--- src/main/java/org/jkan997/booklibrary/htl/BooklistModel.java
+++ src/main/java/org/jkan997/booklibrary/htl/BooklistModel.java
@@ -1,17 +1,53 @@
 package org.jkan997.booklibrary.htl;
 
 import java.util.Date;
-import org.apache.sling.api.SlingHttpServletRequest;
+import javax.jcr.Repository;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.jcr.api.SlingRepository;
 import org.apache.sling.models.annotations.Model;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 
-@Model(adaptables = SlingHttpServletRequest.class)
+@Model(adaptables = Resource.class)
 public class BooklistModel {
+
+    public Repository getRepository() {
+        BundleContext bundleContext = FrameworkUtil.getBundle(BooklistModel.class).getBundleContext();
+        ServiceReference factoryRef = bundleContext.getServiceReference(SlingRepository.class.getName());
+        SlingRepository res = (SlingRepository) bundleContext.getService(factoryRef);
+        return res;
+    }
 
     /*
         YOUR HTL MODEL HERE HERE
-        */
-    
-    public String getDate() {
-        return (new Date()).toString();
+     */
+    public String getSampleBook() {
+        //SlingHttpServletRequest s = this.adaptTo(SlingHttpServletRequest.class);
+        return "A!" + (new Date()).toString() + " '" + getRepository();
     }
 }
+
+/*package org.jkan997.booklibrary.htl;
+
+import java.util.Date;
+import javax.inject.Inject;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.adapter.SlingAdaptable;
+import org.apache.sling.jcr.api.SlingRepository;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = SlingHttpServletRequest.class)
+public class BooklistModel  extends  SlingAdaptable {
+
+
+    public String getDate() {
+        SlingHttpServletRequest s = this.adaptTo(SlingHttpServletRequest.class);
+        return (new Date()).toString() + " '" + s ;
+
+    }
+
+   
+}
+ */
