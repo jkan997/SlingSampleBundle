ce77d07a6b8e69a65611ca02cf30271f34e22a39
jkan997 <jakub.kaniewsky@gmail.com>
2017-03-13 13:51:05 +0100

    Updated pom.xml

--- src/main/java/org/jkan997/booklibrary/htl/Book.java
+++ src/main/java/org/jkan997/booklibrary/htl/Book.java
@@ -5,10 +5,31 @@
  */
 package org.jkan997.booklibrary.htl;
 
-/**
- *
- * @author jakaniew
- */
+import javax.inject.Inject;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class)
 public class Book {
+
+    @Inject
+    private String author;
+    
+    @Inject
+    private String title;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "author=" + author + ", title=" + title + '}';
+    }
+    
     
 }
