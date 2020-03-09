2115a2cdd4ca0ea8bb8e1d0521594123b872c813
Abhishek Arora <abhishekarora12@gmail.com>
2020-03-09 17:52:40 +0530

    Added book model

---
+++ src/main/java/org/jkan997/booklibrary/htl/Book.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jkan997.booklibrary.htl;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class)
+public class Book {
+
+    @ValueMapValue(name = "author")
+    private String author;
+    
+    @ValueMapValue(name = "title")
+    private String title;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "author=" + author + ", title=" + title + '}';
+    }
+    
+    
+}
